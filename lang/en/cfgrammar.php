0be79c2bec15f4ab604e92b796ebeb1a4d18c50f
Posa Levente <h376512@stud.u-szeged.hu>
2026-05-08 19:54:45 +0200

    Modulename fix

--- lang/en/cfgrammar.php
+++ lang/en/cfgrammar.php
@@ -1,1 +1,6 @@
 <?php
+
+$string['pluginname'] = 'CF_Grammar';
+$string['modulename'] = 'CF_Grammar';
+$string['modulename_help'] = 'placeholder';
+$string['modulenameplural'] = 'CF_Grammars';
--- lang/hu/cfgrammar.php
+++ lang/hu/cfgrammar.php
@@ -1,1 +1,6 @@
 <?php
+
+$string['pluginname'] = 'CF_Nyelvtan';
+$string['modulename'] = 'CF_Nyelvtan';
+$string['modulename_help'] = 'placeholder';
+$string['modulenameplural'] = 'CF_Nyelvtanok';
